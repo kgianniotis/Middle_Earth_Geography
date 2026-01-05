fe07d3d3536101791617909e30c9b86753a27b8a
gianniotisk <ukostasu@gmail.com>
2026-01-05 18:39:35 +0200

    Add navbar and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MapPageComponent } from './map-page/map-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'map', component: MapPageComponent },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/map-page/map-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-page',
+  imports: [],
+  templateUrl: './map-page.component.html',
+  styleUrl: './map-page.component.scss'
+})
+export class MapPageComponent {
+
+}
